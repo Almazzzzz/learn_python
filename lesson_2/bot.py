206a8869dc9f2d9d3f3b465f671cc85a36257924
Almaz Badrtdinov <badrtdinov_a@mail.ru>
2018-05-15 13:06:37 +0300

    feat: add using external modules solved task

---
+++ lesson_2/bot.py
@@ -0,0 +1,50 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import ephem
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    mybot = Updater('535442374:AAFD2sWosXO36zxRxG95FJ9OM7y0sL25aHc', request_kwargs=PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', get_constellation, pass_args=True))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def get_constellation(bot, update, args):
+    if args:
+        try:
+            planet_name = args[0].capitalize()
+            planet = getattr(ephem, planet_name)()
+            planet.compute()
+            constellation = ephem.constellation(planet)
+            text = f'{planet_name} сейчас находится в созвездии {constellation[-1]}'
+        except AttributeError:
+            text = 'Такой планеты не существует'
+    else:
+        text = 'Не указано название планеты'
+
+    print(args)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+main()
