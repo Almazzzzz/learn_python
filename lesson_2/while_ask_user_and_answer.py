d41e1d9823389ac5e920f0e172f866675e2f5537
Almaz Badrtdinov <badrtdinov_a@mail.ru>
2018-05-15 13:05:23 +0300

    feat: add while loop solved tasks

---
+++ lesson_2/while_ask_user_and_answer.py
@@ -0,0 +1,16 @@
+answers = { 'привет': 'И тебе привет!', 'как дела': 'Лучше всех', 'пока': 'Увидимся!', 'хорошо': 'Вот и славно!' }
+stop_words = ['хорошо', 'пока']
+
+def get_answer(question):
+    return answers.get(question.lower(), 'Я могу продолжать это вечно...')
+
+def ask_user():
+    while True:
+        user_say = input('Как дела?\n').lower()
+        answer = get_answer(user_say)
+        print(answer)
+
+        if user_say in stop_words:
+            break
+
+ask_user()
